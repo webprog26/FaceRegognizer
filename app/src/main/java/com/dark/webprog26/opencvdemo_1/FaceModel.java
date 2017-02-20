6db3bbc230399876b06fbf4f6b1c8e47d8da7352
webprog26@gmail.com <webprog26@gmail.com>
2017-02-20 14:24:25 +0200

    com.github.wihoho:face-recognition:1.0 lib added, FaceModel.java class created

---
+++ app/src/main/java/com/dark/webprog26/opencvdemo_1/FaceModel.java
@@ -0,0 +1,50 @@
+package com.dark.webprog26.opencvdemo_1;
+
+/**
+ * Created by webpr on 20.02.2017.
+ */
+
+public class FaceModel {
+
+    private long mId;
+    private String mDescription;
+    private String mImageAddr;
+
+    public long getId() {
+        return mId;
+    }
+
+    public String getDescription() {
+        return mDescription;
+    }
+
+    public String getImageAddr() {
+        return mImageAddr;
+    }
+
+    public static Builder newBuilder(){
+        return new FaceModel(). new Builder();
+    }
+
+    class Builder{
+
+        public Builder setId(long id){
+            FaceModel.this.mId = id;
+            return this;
+        }
+
+        public Builder setDescription(String description){
+            FaceModel.this.mDescription = description;
+            return this;
+        }
+
+        public Builder setImageAddr(String imageAddr){
+            FaceModel.this.mImageAddr = imageAddr;
+            return this;
+        }
+
+        public FaceModel build(){
+            return FaceModel.this;
+        }
+    }
+}
